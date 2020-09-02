5bc3b8597ba68486a04d962a9284e9519d112f5f
MoysesAz <m.mirazev@gmail.com>
2020-09-02 15:48:49 -0300

    add methods and change in init

--- Statistic.py
+++ Statistic.py
@@ -3,31 +3,55 @@ import pandas as pd
 import math
 
 
+
 # gerente - > modulos com partes da estatistica
-
 class Statistic:
-    def __init__(self, chave):
-        self.value_inf = [12.5, 13.5, 14.5, 15.5, 16.5, 17.5]
-        self.value_sup = [13.5, 14.5, 15.5, 16.5, 17.5, 18.5]
-        self.value_class = [f"{self.value_inf[i]}|--{self.value_sup[i]} " for i in range(0, len(self.value_inf))]
-
-        self.xi = [(self.value_inf[i] + self.value_sup[i]) / 2 for i in range(0, len(self.value_inf))]
-        self.xi_sqrt = [i * i for i in self.xi]
-
-        if chave == 0:
-            self.yi = [13.5, 14.5, 15.5, 16.5, 17.5, 18.5]
+    """
+        0 = variance table
+        1 = cumulative frequency table by class
+        2 = cumulative frequency table by class
+    """
+    def __init__(self, key):
+        if key == 0:
+            self.xi = [7, 5, 6, 6, 6, 4, 8, 6, 9, 3]
+            self.xi_sqrt = [i * i for i in self.xi]
+            self.yi = [20, 14, 18, 18, 17, 11, 25, 19, 26, 10]
             self.yi_sqrt = [i * i for i in self.yi]
             self.xiyi = [self.xi[i] * self.yi[i] for i in range(0, len(self.xi))]
+            self.tables(key)
 
-        self.fi = [7, 13, 19, 24, 15, 2]
-        self.fixi = [self.xi[i] * self.fi[i] for i in range(0, len(self.xi))]
-        self.far = self.weighted_sum(self.fi)[0]
-        self.wi = [i - self.xi[0] for i in self.xi]
-        self.wi_sqrt = [i * i for i in self.wi]
-        self.fiwi = [self.fi[i] * self.wi[i] for i in range(0, len(self.xi))]
-        self.fiwi_sqrt = [self.fi[i] * self.wi_sqrt[i] for i in range(0, len(self.xi))]
-        self.fr = [i / self.far[1] for i in self.fi]
-        self.frac = self.weighted_sum(self.fr)[0]
+        if key == 1:
+            self.value_inf = [12.5, 13.5, 14.5, 15.5, 16.5, 17.5]
+            self.value_sup = [13.5, 14.5, 15.5, 16.5, 17.5, 18.5]
+            self.value_class = [f"{self.value_inf[i]}|--{self.value_sup[i]} " for i in range(0, len(self.value_inf))]
+            self.xi = [(self.value_inf[i] + self.value_sup[i]) / 2 for i in range(0, len(self.value_inf))]
+            self.xi_sqrt = [i * i for i in self.xi]
+            self.fi = [7, 13, 19, 24, 15, 2]
+            self.fixi = [self.xi[i] * self.fi[i] for i in range(0, len(self.xi))]
+            self.far = self.weighted_sum(self.fi)[0]
+            self.wi = [i - self.xi[0] for i in self.xi]
+            self.wi_sqrt = [i * i for i in self.wi]
+            self.fiwi = [self.fi[i] * self.wi[i] for i in range(0, len(self.xi))]
+            self.fiwi_sqrt = [self.fi[i] * self.wi_sqrt[i] for i in range(0, len(self.xi))]
+            self.fr = [i / self.far[1] for i in self.fi]
+            self.frac = self.weighted_sum(self.fr)[0]
+            self.tables(key)
+
+        if key == 2:
+            self.xi = [14]
+            self.xi_sqrt = [i * i for i in self.xi]
+            self.fi = [7, 13, 19, 24, 15, 2]
+            self.fixi = [self.xi[i] * self.fi[i] for i in range(0, len(self.xi))]
+            self.far = self.weighted_sum(self.fi)[0]
+            self.wi = [i - self.xi[0] for i in self.xi]
+            self.wi_sqrt = [i * i for i in self.wi]
+            self.fiwi = [self.fi[i] * self.wi[i] for i in range(0, len(self.xi))]
+            self.fiwi_sqrt = [self.fi[i] * self.wi_sqrt[i] for i in range(0, len(self.xi))]
+            self.fr = [i / self.far[1] for i in self.fi]
+            self.frac = self.weighted_sum(self.fr)[0]
+            self.tables(key)
+
+
 
     def generator(self, chave):
         for i in range(0, 5):
@@ -37,6 +61,7 @@ class Statistic:
             assert isinstance(frequency, int) or isinstance(frequency, float)
             self.xi.append(value)
             self.fi.append(frequency)
+
 
     @staticmethod
     def weighted_sum(value):
@@ -48,8 +73,10 @@ class Statistic:
             vector.append(sum)
         return [vector, sum]
 
+
     def data_average(self):
         return self.summation["Σ(fixi)"] / self.summation["Σ(fi)"]
+
 
     def mode(self):
         ind_mo = np.argmax(self.table['fi'])
@@ -109,30 +136,28 @@ class Statistic:
             percentis.append(self.value_inf[intervalo_mediana] + ((N - limit_inf) / fi) * h)
         return percentis
 
-    def tables(self, number_table=1):
-        if number_table == 0:
-            self.table = {"Classe": self.value_class,
-                          "xi": self.xi,
-                          "fi": self.fi,
-                          "fixi": self.fixi,
-                          "Fac": self.far,
-                          "fr": self.fr,
-                          "frac": self.frac}
+
+    def tables(self, key=1):
+        if key == 0:
+            self.table = {"xi": self.xi,
+                          "yi": self.yi,
+                          "xi²": self.xi_sqrt,
+                          "yi²": self.yi_sqrt,
+                          "xiyi": self.xiyi}
 
             self.summation = {"Index": "Value",
                               "Σ(xi)": self.weighted_sum(self.xi)[1],
-                              "Σ(fi)": self.weighted_sum(self.fi)[1],
-                              "Σ(fixi)": self.weighted_sum(self.fixi)[1],
-                              "Fac": None,
-                              "Σ(fr)": self.weighted_sum(self.fr)[1],
-                              "frac": None}
+                              "Σ(yi)": self.weighted_sum(self.yi)[1],
+                              "Σ(xi²)": self.weighted_sum(self.xi_sqrt)[1],
+                              "Σ(yi²)": self.weighted_sum(self.yi_sqrt)[1],
+                              "Σ(xiyi)": self.weighted_sum(self.xiyi)[1]}
 
             self.df = pd.DataFrame(self.table)
             self.df_00 = pd.DataFrame(self.summation, index=[0])
             print(self.df)
             print(self.df_00)
 
-        elif number_table == 1:
+        elif key == 1:
             self.table = {"Classe": self.value_class,
                           "xi": self.xi,
                           "fi": self.fi,
@@ -162,46 +187,71 @@ class Statistic:
             print(self.df)
             print(self.df_00)
 
-        elif number_table == 2:
+        elif key == 2:
             self.table = {"Classe": self.value_class,
                           "xi": self.xi,
-                          "yi": self.yi,
-                          "xi²": self.xi_sqrt,
-                          "yi²": self.yi_sqrt,
-                          "xiyi": self.xiyi}
+                          "fi": self.fi,
+                          "fixi": self.fixi,
+                          "Fac": self.far,
+                          "fr": self.fr,
+                          "frac": self.frac}
 
             self.summation = {"Index": "Value",
                               "Σ(xi)": self.weighted_sum(self.xi)[1],
-                              "Σ(yi)": self.weighted_sum(self.fi)[1],
-                              "Σ(xi²)": self.weighted_sum(self.xi_sqrt)[1],
-                              "Σ(yi²)": self.weighted_sum(self.yi_sqrt)[1],
-                              "Σ(xiyi)": self.weighted_sum(self.xiyi)[1]}
+                              "Σ(fi)": self.weighted_sum(self.fi)[1],
+                              "Σ(fixi)": self.weighted_sum(self.fixi)[1],
+                              "Fac": None,
+                              "Σ(fr)": self.weighted_sum(self.fr)[1],
+                              "frac": None}
 
-        self.df = pd.DataFrame(self.table)
-        self.df_00 = pd.DataFrame(self.summation, index=[0])
-        print(self.df)
-        print(self.df_00)
+            self.df = pd.DataFrame(self.table)
+            self.df_00 = pd.DataFrame(self.summation, index=[0])
+            print(self.df)
+            print(self.df_00)
+
 
     def generator_csv(self):
         pass
 
-    def variance(self, vector):
-        vector_01 = [i * i for i in vector]
-        n = len(vector)
-        a = 1 / (n - 1)
-        return (self.weighted_sum(vector_01)[1] - (1 / n) * (self.weighted_sum(vector)[1] ** 2))
 
-    def variance_pearson(self, vector):
-        vector_01 = [i * i for i in vector]
-        n = len(vector)
-        a = 1 / (n - 1)
-        return (1 / n - 1) * (self.weighted_sum(vector_01)[1] - (1 / n) * (self.weighted_sum(vector)[1] ** 2))
+    def variance_x(self):
+        n = len(self.xi)
+        return (self.weighted_sum(self.xi_sqrt)[1] - (1 / n) * (self.weighted_sum(self.xi)[1] ** 2))/n
 
+
+    def variance_y(self):
+        n = len(self.yi)
+        return (self.weighted_sum(self.yi_sqrt)[1] - (1 / n) * (self.weighted_sum(self.yi)[1] ** 2))/n
+
+    #alterações na covariancia de pearson / n assim como as variancias normais
     def r_pearson(self):
-        pass
+        n = len(self.xi)
+        sum_xiyi = self.weighted_sum(self.xiyi)[1]
+        sum_xi = self.weighted_sum(self.xi)[1]
+        sum_yi = self.weighted_sum(self.yi)[1]
+        cov_xy = (sum_xiyi - (1/n)*sum_xi*sum_yi)/n
+        var_x = self.variance_x()
+        var_y = self.variance_y()
+        return cov_xy/math.pow(var_x * var_y, 1/2)
 
-    def standard_deviation(self, variance):
-        return math.pow(variance, 1 / 2)
+
+    def classification_r_pearson(self):
+        value = self.r_pearson()
+        if 0 <= value <= 0.19:
+            return "Muito Fraca"
+        elif 0.20 <= value <= 0.39:
+            return "Fraca"
+        elif 0.40<= value <= 0.69:
+            return "Moderada"
+        elif 0.70<= value <= 0.89:
+            return "Forte"
+        elif 0.90<= value <= 1:
+            return "Muito Forte"
+
+
+    def standard_deviation(self):
+        return math.pow(self.variance(), 1/2)
+
 
     def covariance(self, vector_x, vector_y):
         pass
@@ -209,11 +259,7 @@ class Statistic:
 
 if __name__ == '__main__':
     objeto = Statistic(0)
-    objeto.tables(2)
-    print(f"variance: {objeto.variance(objeto.xi)}")
-    print(f"variance: {objeto.variance_pearson(objeto.xi)}")
-    print(f"variance: {objeto.variance(objeto.yi)}")
-    print(f"variance: {objeto.variance_pearson(objeto.yi)}")
-
-
+    print(f"variance: {objeto.variance_x()}")
+    print(f"covariancia: {objeto.r_pearson()}")
+    print(objeto.classification_r_pearson())
 
